53dd5e398251f813e77bc739be0316036fa4ff78
jae <jsmchugh@gmail.com>
2019-09-22 10:37:05 +1000

    Bubble tea class handles whether straw is in or not.

---
+++ Assets/_inkjam19/Scripts/BubbleTea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTea : MonoBehaviour
+{
+	[SerializeField]
+	bool strawIn = true;
+
+	[Header("Submodels")]
+
+	[SerializeField]
+	GameObject strawObject;
+
+	void Start()
+	{
+		if(!strawIn)
+		{
+			strawObject.SetActive(false);
+		}
+	}
+}
