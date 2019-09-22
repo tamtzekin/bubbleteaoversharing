b6a275ac54cd8c8b3f7e5ced891e385f3fd545f6
jae <jsmchugh@gmail.com>
2019-09-22 10:44:14 +1000

    Bubble tea - can remove lid with code.

--- Assets/_inkjam19/Scripts/BubbleTea.cs
+++ Assets/_inkjam19/Scripts/BubbleTea.cs
@@ -7,16 +7,27 @@ public class BubbleTea : MonoBehaviour
 	[SerializeField]
 	bool strawIn = true;
 
+	[SerializeField]
+	bool lidOn = true;
+
 	[Header("Submodels")]
 
 	[SerializeField]
 	GameObject strawObject;
+
+	[SerializeField]
+	GameObject lidObject;
 
 	void Start()
 	{
 		if(!strawIn)
 		{
 			strawObject.SetActive(false);
+		}
+
+		if (!lidOn)
+		{
+			lidObject.SetActive(false);
 		}
 	}
 }
