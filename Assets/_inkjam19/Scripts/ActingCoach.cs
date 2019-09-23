48ce86f2984ff61e04ef5631428b3132720cfa4f
jae <jsmchugh@gmail.com>
2019-09-23 19:52:18 +1000

    Acting Coach - manages actors. Initial pass from this morning.

---
+++ Assets/_inkjam19/Scripts/ActingCoach.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActingCoach : MonoBehaviour
+{
+	Animator[] animators;
+
+	void Start()
+	{
+		animators = FindObjectsOfType<Animator>();
+		foreach (Animator animator in animators)
+		{
+			animator.gameObject.SetActive(false);
+		}
+	}
+}
